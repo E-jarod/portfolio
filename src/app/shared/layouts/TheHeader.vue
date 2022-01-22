67fbd92ae825dcd40b0081f1ea12fd752b79bb5d
E-jarod <ejilane.jarod@gmail.com>
2022-01-22 12:47:52 +0100

    navbar UI ok, but not finished

--- src/app/shared/layouts/TheHeader.vue
+++ src/app/shared/layouts/TheHeader.vue
@@ -4,8 +4,12 @@ import HeaderRoutes from './HeaderRoutes.vue';
 </script>
 
 <template>
-  <div class="flex justify-between py-8 px-8">
-    <HeaderLogo />
-    <HeaderRoutes />
+  <div class="px-4 sm:px-6 md:px-8">
+    <div
+      class="relative pt-6 lg:pt-8 flex items-center justify-between text-slate-700 font-semibold text-sm leading-6 dark:text-slate-200"
+    >
+      <HeaderLogo />
+      <HeaderRoutes />
+    </div>
   </div>
 </template>
