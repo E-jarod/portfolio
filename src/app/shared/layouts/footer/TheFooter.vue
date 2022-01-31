6e8ccde66d2e23feb2d089c8ee0b74881ae1a3c7
E-jarod <ejilane.jarod@gmail.com>
2022-01-31 16:02:54 +0100

    footer finished

--- src/app/shared/layouts/footer/TheFooter.vue
+++ src/app/shared/layouts/footer/TheFooter.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="flex flex-shrink-0 print:hidden">
-    <div>test</div>
+  <footer
+    class="flex items-end justify-center space-x-10 text-xs text-slate-400 dark:text-slate-500 print:hidden"
+  >
+    <span>EJILANE Jarod</span>
+    <span>Made with TailwindCSS, Vue 3 and Vite</span>
   </footer>
 </template>
