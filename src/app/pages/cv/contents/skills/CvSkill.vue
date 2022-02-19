be1f01e1dfac06dbfe94700fd4a0d28834c82d4b
E-jarod <ejilane.jarod@gmail.com>
2022-02-19 09:05:03 +0100

    removed useless div

--- src/app/pages/cv/contents/skills/CvSkill.vue
+++ src/app/pages/cv/contents/skills/CvSkill.vue
@@ -34,41 +34,39 @@ defineProps<CvSkillProps>();
 </script>
 
 <template>
-  <li>
+  <li
+    class="inline-flex items-center w-full space-x-10vw"
+    :class="{ 'border-b-2 border-red-100 mb-2vw pb-2vw': separator }"
+  >
     <div
-      class="inline-flex items-center w-full space-x-10vw"
-      :class="{ 'border-b-2 border-red-100 mb-2vw pb-2vw': separator }"
+      class="inline-flex items-center justify-center rounded-full w-12vw h-12vw"
+      :class="[bgColor, padding, { 'p-2vw': !padding }]"
     >
-      <div
-        class="inline-flex items-center justify-center rounded-full w-12vw h-12vw"
-        :class="[bgColor, padding, { 'p-2vw': !padding }]"
+      <svg
+        v-if="isSvg && svgIcon"
+        class="inline-block stroke-current stroke-0 h-8vw w-8vw"
+        :class="[
+          svgIcon.color,
+          { 'rounded-sm md:rounded-md': svgIcon.rounded },
+        ]"
       >
-        <svg
-          v-if="isSvg && svgIcon"
-          class="inline-block stroke-current stroke-0 h-8vw w-8vw"
-          :class="[
-            svgIcon.color,
-            { 'rounded-sm md:rounded-md': svgIcon.rounded },
-          ]"
-        >
-          <symbol :id="svgIcon.id" viewBox="0 0 32 32">
-            <path :d="svgIcon.path"></path>
-          </symbol>
-          <use :xlink:href="`#${svgIcon.id}`"></use>
-        </svg>
-        <fa-icon
-          v-else-if="!isSvg && faIcon"
-          class="w-8vw h-8vw"
-          :class="[faIcon.color]"
-          :icon="faIcon.icon"
-        ></fa-icon>
-        <span v-else>ERR</span>
-      </div>
-      <span
-        class="text-xlvw"
-        :class="{ 'font-bold': isConfirmedSkill }"
-        v-text="skillName"
-      ></span>
+        <symbol :id="svgIcon.id" viewBox="0 0 32 32">
+          <path :d="svgIcon.path"></path>
+        </symbol>
+        <use :xlink:href="`#${svgIcon.id}`"></use>
+      </svg>
+      <fa-icon
+        v-else-if="!isSvg && faIcon"
+        class="w-8vw h-8vw"
+        :class="[faIcon.color]"
+        :icon="faIcon.icon"
+      ></fa-icon>
+      <span v-else>ERR</span>
     </div>
+    <span
+      class="text-xlvw"
+      :class="{ 'font-bold': isConfirmedSkill }"
+      v-text="skillName"
+    ></span>
   </li>
 </template>
