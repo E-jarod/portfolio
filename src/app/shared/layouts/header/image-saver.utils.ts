6a1724c18dfa2db682539449ed30ce6f8b679ef2
E-jarod <ejilane.jarod@gmail.com>
2022-02-21 00:47:19 +0100

    better pp image

--- src/app/shared/layouts/header/image-saver.utils.ts
+++ src/app/shared/layouts/header/image-saver.utils.ts
@@ -4,7 +4,7 @@
 // https://stackoverflow.com/questions/22932422/get-github-avatar-from-email-or-name
 
 export const updateProfileImage = async (
-  sizeInPixels = 48,
+  sizeInPixels = 32,
 ): Promise<string> => {
   const { url } = await fetch(
     `https://avatars.githubusercontent.com/E-jarod?size=${sizeInPixels}`,
