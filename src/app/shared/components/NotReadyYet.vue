1bfe5580d3129318f6c4bfe70ce3e8be30e7759d
E-jarod <ejilane.jarod@gmail.com>
2022-03-08 00:18:09 +0100

    small layout change

--- src/app/shared/components/NotReadyYet.vue
+++ src/app/shared/components/NotReadyYet.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="flex flex-col items-center justify-around pt-60 sm:pt-48 md:pt-32 lg:pt-28 xl:pt-20"
+    class="flex flex-col-reverse items-center justify-around pt-60 sm:pt-48 md:pt-32 lg:pt-28 xl:pt-20"
   >
-    <h1 class="mb-10 text-3xl font-extrabold md:mb-5">Not ready yet...</h1>
+    <h1 class="text-3xl font-extrabold">Not ready yet...</h1>
     <img src="/wip.svg" alt="page not ready yet" />
   </div>
 </template>
