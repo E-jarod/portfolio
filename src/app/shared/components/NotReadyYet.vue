ec02a175a47fb0a1ad5428461043485b33f2e0d9
E-jarod <ejilane.jarod@gmail.com>
2022-03-08 00:16:36 +0100

    404 page

--- src/app/shared/components/NotReadyYet.vue
+++ src/app/shared/components/NotReadyYet.vue
@@ -3,6 +3,6 @@
     class="flex flex-col items-center justify-around pt-60 sm:pt-48 md:pt-32 lg:pt-28 xl:pt-20"
   >
     <h1 class="mb-10 text-3xl font-extrabold md:mb-5">Not ready yet...</h1>
-    <img src="/wip.svg" alt="test" />
+    <img src="/wip.svg" alt="page not ready yet" />
   </div>
 </template>
