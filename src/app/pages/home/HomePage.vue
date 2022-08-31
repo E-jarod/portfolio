f884a99c609bffa3888a78f8dffdc875c130dfec
E-jarod <e.jarod.dev@gmail.com>
2022-08-31 15:53:54 +0200

    small modifs ?

--- src/app/pages/home/HomePage.vue
+++ src/app/pages/home/HomePage.vue
@@ -28,7 +28,7 @@
         class="flex items-center justify-center w-40 h-12 px-0 font-semibold text-white rounded-2xl bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 sm:w-48 md:w-60 sm:px-6 dark:bg-red-500 dark:highlight-white/20 dark:hover:bg-red-400"
         to="/skills"
       >
-        View my projects
+        See my projects
       </router-link>
       <a
         class="flex items-center justify-center w-40 h-12 px-0 font-semibold bg-white shadow-sm rounded-2xl sm:px-6 ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-red-500 text-slate-400 sm:w-44 md:w-52 dark:bg-slate-800 dark:ring-0 dark:text-slate-300 dark:highlight-white/5 dark:hover:bg-slate-700"
--- src/app/pages/skills/SkillsPage.vue
+++ src/app/pages/skills/SkillsPage.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-import WorkInProgress from '@app/shared/components/WorkInProgress.vue';
+// import WorkInProgress from '@app/shared/components/WorkInProgress.vue';
+// number 18
+// number 20
+// number 27
+// https://learntocodewith.me/posts/portfolio-tips/
 </script>
 
 <template>
-  <WorkInProgress />
+  <h1 class="text-3xl font-bold">My favorite workflow</h1>
+  <ul>
+    <li>Step 1</li>
+    <li>Step 2</li>
+    <li>Step 3</li>
+  </ul>
+  <!-- <WorkInProgress /> -->
 </template>
